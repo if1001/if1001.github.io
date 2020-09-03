c5c380f63018ab9a5a22d9d4305d597f98627cc2
Leopoldo Teixeira <lmt@cin.ufpe.br>
2020-09-03 10:52:39 -0300

    código visto em aula no dia 02.09.2020

--- 2020-08-26/testing/app/src/main/java/br/ufpe/cin/android/testing/MainActivity.kt
+++ 2020-08-26/testing/app/src/main/java/br/ufpe/cin/android/testing/MainActivity.kt
@@ -2,11 +2,26 @@ package br.ufpe.cin.android.testing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        botao.setOnClickListener(View.OnClickListener {
+
+        })
+        botao.setOnClickListener {
+            val b = it as Button
+            Log.d("AULA",b.text.toString())
+            val oQueFoiDigitado = campoDeTexto.text
+            textoExibido.text = oQueFoiDigitado
+        }
+
+
     }
 }
---
+++ 2020-08-26/testing/app/src/main/java/br/ufpe/cin/android/testing/OutraActivity.kt
@@ -0,0 +1,11 @@
+package br.ufpe.cin.android.testing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OutraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_outra)
+    }
+}
