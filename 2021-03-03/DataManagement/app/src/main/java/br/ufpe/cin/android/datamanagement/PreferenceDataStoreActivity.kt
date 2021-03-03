cb451ee76d46b8b34c845f381d920e91208ca797
Leopoldo Teixeira <lmt@cin.ufpe.br>
2021-03-03 10:34:42 -0300

    starter code

---
+++ 2021-03-03/DataManagement/app/src/main/java/br/ufpe/cin/android/datamanagement/PreferenceDataStoreActivity.kt
@@ -0,0 +1,11 @@
+package br.ufpe.cin.android.datamanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreferenceDataStoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preference)
+    }
+}
---
+++ 2021-03-03/DataManagement/app/src/main/java/br/ufpe/cin/android/datamanagement/PreferenceScreenActivity.kt
@@ -0,0 +1,11 @@
+package br.ufpe.cin.android.datamanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreferenceScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preference_screen)
+    }
+}
